bf8889db6850818fe9676af32c20df14c7d506c6
Pedro <pedrohenben2014@gmail.com>
2024-12-21 16:33:30 -0300

    Add passo 3 mediana das medianas

--- 4.MedianofTwoSortedArrays/4.Median_of _Two_Sorted_Arrays.py
+++ 4.MedianofTwoSortedArrays/4.Median_of _Two_Sorted_Arrays.py
@@ -10,15 +10,25 @@ class Solution:
             print(f"Array pequeno ordenado: {A}")
             return A[k]  
         
-        # Divisão da lista para tamanho 5
+        # Divisao da lista para tamanho 5
         sublistas = [A[i:i + 5] for i in range(0, len(A), 5)]
         print(f"Sublistas de 5 elementos: {sublistas}")
 
         medianas = [sorted(sublista)[len(sublista) // 2] for sublista in sublistas]
         print(f"Medianas de cada sublista: {medianas}")
 
+        pivo = self.mediana_das_medianas(medianas, len(medianas) // 2)
+        print(f"Pivo (mediana das medianas): {pivo}")
+        
+        menores = [x for x in A if x < pivo]
+        maiores = [x for x in A if x > pivo]
+        pivots = [x for x in A if x == pivo]
+        print(f"Menores: {menores}, Pivos: {pivots}, Maiores: {maiores}")
+
         return self.mediana_das_medianas(medianas, len(medianas) // 2) 
-#TesteMediana
+    
+    
+#Teste
 solucao = Solution()
-resultado = solucao.mediana_das_medianas([9, 3, 7, 2, 6, 5, 8, 1, 4, 11, 18], 2) #Ex aula
+resultado = solucao.mediana_das_medianas([9, 3, 7, 2, 6, 5, 8, 1, 4, 11, 18], 2) #Ex Aula
 print(f"Resultado: {resultado}")
