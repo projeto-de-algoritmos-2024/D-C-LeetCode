6c06ecc79630a447a0cf284e13e340bd5ebb280d
leomitx10 <leandroalmeidaoliveira650@gmail.com>
2024-12-21 22:02:56 -0300

    add multiplicacoes

--- 43. MultiplyStrings/43. MultiplyStrings.py
+++ 43. MultiplyStrings/43. MultiplyStrings.py
@@ -12,17 +12,18 @@ class Solution:
         tamanhoMax = max(len(num1), len(num2))
         metade = tamanhoMax // 2
         
-        num1, num2 = num1.zfill(tamanhoMax), num2.zfill(tamanhoMax)  
+        num1, num2 = num1.zfill(tamanhoMax), num2.zfill(tamanhoMax)
+
+        x1, x0 = num1[:-metade], num1[-metade:]
+        y1, y0 = num2[:-metade], num2[-metade:]
+
+        z0 = self.karatsuba(x0, y0)  
+        z2 = self.karatsuba(x1, y1)  
+        z1 = self.karatsuba(str(int(x0) + int(x1)), str(int(y0) + int(y1)))  
         
-        x1, x0 = num1[:-metade], num1[-metade:]  
-        y1, y0 = num2[:-metade], num2[-metade:]  
-        
-        print(f"Dividido num1: {num1} -> x1: {x1}, x0: {x0}")
-        print(f"Dividido num2: {num2} -> y1: {y1}, y0: {y0}")
+        print(f"z0: {z0}, z2: {z2}, z1: {z1}")
         
         return "Produto intermediario"
 
-sol = Solution()
-
-print("Resultado para '1234' e '5678':")
-print(sol.multiply("1234", "5678"))
+solution = Solution()
+print(solution.multiply("1234", "5678"))  
